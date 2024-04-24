2484f915692130632f392b10a0fbbe536c5e70a1
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-04-24 19:06:30 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -13,5 +13,4 @@ all_mvt = conn.read(worksheet="All_mvmt")
 progression = conn.read(worksheet="Progression")
 list_rm = [1,3,5,10]
 
-dico_ex = all_mvt.groupby('Category').agg({'Exercice':list(lambda x :x.unique())})
-dico_ex = dict(zip(dico_ex['Category'], dico_ex['Exercice']))
+dico_ex = all_mvt.groupby('Category')['Exercice'].unique().apply(list).to_dict()
