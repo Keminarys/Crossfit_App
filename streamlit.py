8dc6f3acde65a3f038088ecfbd25a3d68aab5d66
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-04-24 19:24:46 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,14 +3,20 @@ import numpy as np
 import datetime
 import re
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import plotly.express as px
 import plotly.graph_objects as go
 
-
-conn = st.connection("gsheets", type=GSheetsConnection)
-all_mvt = conn.read(worksheet="All_mvmt")
-progression = conn.read(worksheet="Progression")
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_name(st.secret, scope)
+gc = gspread.authorize(credentials)
+worksheet = gc.open('Database_CF83').All_mvmt
+all_mvt = pd.DataFrame(worksheet.get_all_records())
 list_rm = [1,3,5,10]
 
 dico_ex = all_mvt.groupby('Category')['Exercice'].unique().apply(list).to_dict()
+st.title('Crossfit83 Le Beausset')
+st.dataframe(all_mvt)
+st.write(dico_ex)
