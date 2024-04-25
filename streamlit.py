5a14fe5b1d753142591f7f6a477597a59aeeb57b
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-04-25 10:57:25 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,23 +2,32 @@ import pandas as pd
 import numpy as np
 import datetime
 import re
-import streamlit as st
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import plotly.express as px
 import plotly.graph_objects as go
 
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-# Set up Google Sheets API credentials
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name(, scope)
-client = gspread.authorize(creds)
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Open the Google Sheets file
-spreadsheet = client.open('Your Google Sheets File Name')
+df = conn.read(worksheet="All_mvmt")
 
-# Get the first worksheet
-worksheet = spreadsheet.get_worksheet(0)
+# Print results.
+st.dataframe(df)
+# # Set up Google Sheets API credentials
+# scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/drive']
+# creds = ServiceAccountCredentials.from_json_keyfile_name(, scope)
+# client = gspread.authorize(creds)
+
+# # Open the Google Sheets file
+# spreadsheet = client.open('Your Google Sheets File Name')
+
+# # Get the first worksheet
+# worksheet = spreadsheet.get_worksheet(0)
 # gc = gspread.service_account(filename=skey)
 # worksheet = gc.open('Database_CF83').All_mvmt
 # all_mvt = pd.DataFrame(worksheet.get_all_records())
