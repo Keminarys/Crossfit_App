e3b9880cc86fce66a146fa2d9ab14a6a133a4ae2
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-04-26 22:52:43 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -15,7 +15,7 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 all_mvmt = conn.read(worksheet="All_mvmt")
 df = conn.read(worksheet="Progression")
 df_name = conn.read(worksheet="Profils")
-st.dataframe(df)
+st.dataframe(all_mvmt)
 list_name = list(df_name["Name"].unique())
 list_name = [x for x in list_name if str(x) != "nan"]
 list_rm = [1,3,5,10]
