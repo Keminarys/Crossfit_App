6b3fcb37776c33929b50e6726971f27fbff30b88
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-04-30 10:22:55 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -88,7 +88,7 @@ with tab3 :
     st.dataframe(df_perso)
 
 with tab4 :
-    edited_obj = st.data_editor(df_obj)
+    edited_obj = st.data_editor(df_obj, num_rows='dynamic')
     if st.button('Mettre à jour mes objectifs') :
         df_obj_edit = conn.update(worksheet="Objectif",data=edited_obj)
         st.cache_data.clear()
