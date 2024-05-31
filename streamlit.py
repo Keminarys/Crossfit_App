b8b1b205ca50eabaf62cedd133d14bf0c2d4f246
Keminarys <115616113+Keminarys@users.noreply.github.com>
2024-05-31 21:38:01 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,7 @@ import datetime
 import re
 import streamlit as st
 import streamlit_authenticator as stauth
-from streamlit_authenticator.utilities import Hasher
+from streamlit_authenticator.utilities.Hasher import generate
 from streamlit_gsheets import GSheetsConnection
 import plotly.express as px
 import plotly.graph_objects as go
@@ -79,7 +79,7 @@ if st.session_state["authentication_status"]:
             st.rerun()
         with st.sidebar.expander("Reset password") :
             if st.session_state["authentication_status"]:
-                st.write(stauth.Hasher.generate(["c0Nnex1onàl4d4tab4s3!"]))
+                st.write(generate(["c0Nnex1onàl4d4tab4s3!"]))
                 try:
                     if authenticator.reset_password(st.session_state["username"]):
                         st.success('Password modified successfully')
