d3d12337fa10624648b70d16da71cf4b2be2005a
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-02-04 16:21:53 +0100

    Update scheduleResa.py

--- pages/scheduleResa.py
+++ pages/scheduleResa.py
@@ -1,77 +1,13 @@
 import streamlit as st
-from streamlit_calendar import calendar
 from datetime import datetime
 
 # Get the current date and time
 current_date = datetime.now()
 current_date = current_date.date()
 
-events = [
-    {
-        "title": "Event 1",
-        "color": "#FF6C6C",
-        "start": "2023-07-03",
-        "end": "2023-07-05",
-        "resourceId": "a",
-    },
-    {
-        "title": "Event 2",
-        "color": "#FFBD45",
-        "start": "2023-07-01",
-        "end": "2023-07-10",
-        "resourceId": "b",
-    },
-    {
-        "title": "Event 3",
-        "color": "#FF4B4B",
-        "start": "2023-07-20",
-        "end": "2023-07-20",
-        "resourceId": "c",
-    },
-    {
-        "title": "Event 4",
-        "color": "#FF6C6C",
-        "start": "2023-07-23",
-        "end": "2023-07-25",
-        "resourceId": "d",
-    },
-]
+previous_monday = current_date - timedelta(days=current_date.weekday())
 
-calendar_options = {
-    "editable": "true",
-    "navLinks": "true",
-    "selectable": "true",
-}
+# Find the next Sunday
+next_sunday = previous_monday + timedelta(days=6)
 
-calendar_options = {
-            **calendar_options,
-            "headerToolbar": {
-                "left": "today prev,next",
-                "center": "title",
-                "right": "dayGridDay,dayGridWeek,dayGridMonth",
-            },
-            "initialDate": "2023-07-01",
-            "initialView": "dayGridMonth",
-        }
-
-state = calendar(
-    events=st.session_state.get("events", events),
-    options=calendar_options,
-    custom_css="""
-    .fc-event-past {
-        opacity: 0.8;
-    }
-    .fc-event-time {
-        font-style: italic;
-    }
-    .fc-event-title {
-        font-weight: 700;
-    }
-    .fc-toolbar-title {
-        font-size: 2rem;
-    }
-    """,
-    key="daygrid",
-)
-
-st.write(state)
+st.write(previous_monday, current_date, next_sunday)
