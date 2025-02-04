b9c523cbc3d114713472a0f10b99bbf646962585
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-02-04 15:32:15 +0100

    Update scheduleResa.py

--- pages/scheduleResa.py
+++ pages/scheduleResa.py
@@ -2,15 +2,6 @@ import streamlit as st
 
 from streamlit_calendar import calendar
 
-st.set_page_config(page_title="Demo for streamlit-calendar", page_icon="📆")
-
-st.markdown(
-    "## Demo for [streamlit-calendar](https://github.com/im-perativa/streamlit-calendar) 📆"
-)
-
-st.markdown(
-    "[![](https://img.shields.io/github/stars/im-perativa/streamlit-calendar?style=social)](https://github.com/im-perativa/streamlit-calendar)"
-)
 
 mode = st.selectbox(
     "Calendar Mode:",
@@ -163,72 +154,17 @@ calendar_options = {
     "selectable": "true",
 }
 
-if "resource" in mode:
-    if mode == "resource-daygrid":
-        calendar_options = {
-            **calendar_options,
-            "initialDate": "2023-07-01",
-            "initialView": "resourceDayGridDay",
-            "resourceGroupField": "building",
-        }
-    elif mode == "resource-timeline":
-        calendar_options = {
+calendar_options = {
             **calendar_options,
             "headerToolbar": {
                 "left": "today prev,next",
                 "center": "title",
-                "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
-            },
-            "initialDate": "2023-07-01",
-            "initialView": "resourceTimelineDay",
-            "resourceGroupField": "building",
-        }
-    elif mode == "resource-timegrid":
-        calendar_options = {
-            **calendar_options,
-            "initialDate": "2023-07-01",
-            "initialView": "resourceTimeGridDay",
-            "resourceGroupField": "building",
-        }
-else:
-    if mode == "daygrid":
-        calendar_options = {
-            **calendar_options,
-            "headerToolbar": {
-                "left": "today prev,next",
-                "center": "title",
-                "right": "dayGridDay,dayGridWeek,dayGridMonth",
+                "right": "dayGridWeek",
             },
             "initialDate": "2023-07-01",
             "initialView": "dayGridMonth",
         }
-    elif mode == "timegrid":
-        calendar_options = {
-            **calendar_options,
-            "initialView": "timeGridWeek",
-        }
-    elif mode == "timeline":
-        calendar_options = {
-            **calendar_options,
-            "headerToolbar": {
-                "left": "today prev,next",
-                "center": "title",
-                "right": "timelineDay,timelineWeek,timelineMonth",
-            },
-            "initialDate": "2023-07-01",
-            "initialView": "timelineMonth",
-        }
-    elif mode == "list":
-        calendar_options = {
-            **calendar_options,
-            "initialDate": "2023-07-01",
-            "initialView": "listMonth",
-        }
-    elif mode == "multimonth":
-        calendar_options = {
-            **calendar_options,
-            "initialView": "multiMonthYear",
-        }
+
 
 state = calendar(
     events=st.session_state.get("events", events),
@@ -253,7 +189,3 @@ state = calendar(
 if state.get("eventsSet") is not None:
     st.session_state["events"] = state["eventsSet"]
 
-st.write(state)
-
-st.markdown("## API reference")
-st.help(calendar)
