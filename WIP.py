628cf62a99c6956b7b94f3829c98c75bd2980681
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-05-01 23:22:43 +0200

    Update WIP.py

--- WIP.py
+++ WIP.py
@@ -9,9 +9,9 @@ from datetime import date
 
 
 PAGES = {
-    "Profile": profiles_page,
-    "Progession": progress,
-    "Ressources Technique": ressources,
+    "Votre Profil": profiles_page,
+    "Votre Progession": progress,
+    "Ressources Technique Crossfit": ressources,
     "Prog de la semaine": scheduleResa
 }
 
@@ -26,39 +26,52 @@ def get_df(sheet_name) :
     datas = conn.read(worksheet=sheet_name)
     return datas
 
-def display_card(page_name):
+def display_card(page_name, page_link):
     st.markdown(f"""
-    <a href='/{page_name}' style='text-decoration: none;'>
-    <div style='background-color: red; padding: 20px; margin: 10px; border-radius: 0; text-align: center;'>
-    <h2 style='color: white;'>{page_name}</h2>
-    </div>
-    </a>
+        <a href='{page_link}' style="text-decoration: none;'>
+            <div style='
+                background-color: #E63946; 
+                padding: 20px; 
+                margin: 10px; 
+                border-radius: 10px; 
+                text-align: center;
+                font-size: 24px;
+                font-weight: bold;
+                color: white;
+                box-shadow: 5px 5px 10px rgba(0,0,0,0.3);
+                transition: 0.3s ease-in-out;
+                '>
+                {page_name}
+            </div>
+        </a>
     """, unsafe_allow_html=True)
+
 
 def main():
     
-    # conn = get_conn()
-    # df_name = get_df("Profils")
-    # df_name = df_name[['Name']].dropna()
+    conn = get_conn()
+    df_name = get_df("Profils")
+    df_name = df_name[['Name']].dropna()
     title, logo = st.columns([3,1])
     with title :
         st.title('Crossfit83 Le Beausset')
     with logo :
         st.image("LogoCrossfit.jpg")
     st.divider()
-    st.header("Pages")
+    st.header("Menu")
+    st.subheade("Cliquez sur une ")
     pages = list(PAGES.keys())
 
     col1, col2 = st.columns(2) 
     with col1 :
-        display_card(pages[0])
+        display_card(pages[0], f"/{pages[0]}")
     with col2 :
-        display_card(pages[1])
+        display_card(pages[1], f"/{pages[1]}")
     col3, col4 = st.columns(2)    
     with col3 :
-        display_card(pages[2])
+        display_card(pages[2], f"/{pages[2]}")
     with col4 :
-        display_card(pages[3])
+        display_card(pages[3], f"/{pages[3]}")
 if __name__ == "__main__":
     main()
 
