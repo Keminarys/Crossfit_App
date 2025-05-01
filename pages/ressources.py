0a669a35e024954ab79ef19f2ebc8b8bc84d13bd
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-05-01 23:02:08 +0200

    Update ressources.py

--- pages/ressources.py
+++ pages/ressources.py
@@ -76,7 +76,7 @@ def get_all_heroes() :
 
 def wodGirls() :
     wodGirlsPage = get_df("benchmarks")
-    wodGirlsPage = wodGirlsPage[["Benchmark"] == "wodGirls"]["Description"]
+    wodGirlsPage = wodGirlsPage.loc[["Benchmark"] == "wodGirls"]["Description"]
     wodGirls = ast.literal_eval(wodGirlsPage)
     return wodGirls
 conn = get_conn()
