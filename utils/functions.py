89f48623f0a154ecb28120682b76179ad6fddedd
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-05-02 08:34:25 +0200

    Create functions.py

---
+++ utils/functions.py
@@ -0,0 +1,17 @@
+def go_home():
+    button_html = """
+        <div style="position: fixed; top: 10px; right: 10px; z-index: 1000;">
+            <form action="?page=WIP.py" method="get">
+                <button type="submit"
+                    style="background: linear-gradient(45deg, #D62828, #E63946);
+                    color: white; font-size: 18px; font-weight: bold;
+                    padding: 10px 20px; border-radius: 10px; border: none;
+                    cursor: pointer; box-shadow: 0px 5px 10px rgba(0,0,0,0.3);">
+                    🏠 Retour à l'accueil
+                </button>
+            </form>
+        </div>
+    """
+    st.markdown(button_html, unsafe_allow_html=True)
+
+
