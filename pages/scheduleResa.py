0c0b2bf9ec43c1ebe2b016b98b2e91dfdb8181fd
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-07-12 17:03:51 +0200

    Update scheduleResa.py

--- pages/scheduleResa.py
+++ pages/scheduleResa.py
@@ -14,75 +14,76 @@ import streamlit.components.v1 as components
 from utils.auth import login_ui
 from utils.ui_helpers import render_nav_bar
 
-if not st.session_state.get("authenticated"):
-    st.warning("Please log in")
-    login_ui()
+def resaPage():
+    if not st.session_state.get("authenticated"):
+        st.warning("Please log in")
+        login_ui()
+        
+    st.set_page_config(layout="wide")
+    render_nav_bar()
     
-st.set_page_config(layout="wide")
-render_nav_bar()
-
-
-planning = get_conn_and_df("WODSemaine")
-url = planning.iloc[0, 8]
-planning = planning[['WOD', 'Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche']].dropna()
-
-# Get current date and determine Monday of the current week
-today = date.today()
-monday = today - timedelta(days=today.weekday())
-days = [monday + timedelta(days=i) for i in range(7)]
-
-
-daysConvert = {
-    "Monday": "Lundi", "Tuesday": "Mardi", "Wednesday": "Mercredi",
-    "Thursday": "Jeudi", "Friday": "Vendredi", "Saturday": "Samedi",
-    "Sunday": "Dimanche"
-}
-
-# Define example workout details for each day
-# Streamlit UI
-st.subheader("Planning de la semaine :calendar: ")
-
-# Create seven columns for the buttons
-cols = st.columns(7)
-selected_day = None
-
-# Generate buttons in respective columns
-for i, day in enumerate(days):
-    button_label = f"{daysConvert[day.strftime('%A')]} {day.strftime('%d')}"  # Example: "Monday 28"
-    if cols[i].button(button_label):
-        selected_day = daysConvert[day.strftime('%A')]
-
-# Display workout details for the selected day
-if selected_day:
-    st.subheader("Workout of the Day")
-    selected_planning = planning[['WOD', selected_day]]
-    st.markdown("""
-        <style>
-            .card {
-                border-radius: 10px;
-                padding: 20px;
-                margin-bottom: 15px;
-                background-color: #2E3B4E;
-                color: white;
-                text-align: center;
-                font-size: 18px;
-                box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
-            }
-        </style>
-    """, unsafe_allow_html=True)
     
-    # Creating vertical scrolling cards
-    for i in range(len(selected_planning)):
-        with st.container():
-            st.markdown(f"""
-                <div class="card">
-                    <h2>{selected_planning.loc[i, "WOD"]}</h2>
-                    <p>{selected_planning.loc[i, selected_day]}</p>
-                </div>
-            """, unsafe_allow_html=True)
-
-
-st.divider()
-
-st.subheader("Inscription au cours via PollForAll :calendar: ")
-components.iframe(url, height=800, scrolling = True)
+    planning = get_conn_and_df("WODSemaine")
+    url = planning.iloc[0, 8]
+    planning = planning[['WOD', 'Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi','Dimanche']].dropna()
+    
+    # Get current date and determine Monday of the current week
+    today = date.today()
+    monday = today - timedelta(days=today.weekday())
+    days = [monday + timedelta(days=i) for i in range(7)]
+    
+    
+    daysConvert = {
+        "Monday": "Lundi", "Tuesday": "Mardi", "Wednesday": "Mercredi",
+        "Thursday": "Jeudi", "Friday": "Vendredi", "Saturday": "Samedi",
+        "Sunday": "Dimanche"
+    }
+    
+    # Define example workout details for each day
+    # Streamlit UI
+    st.subheader("Planning de la semaine :calendar: ")
+    
+    # Create seven columns for the buttons
+    cols = st.columns(7)
+    selected_day = None
+    
+    # Generate buttons in respective columns
+    for i, day in enumerate(days):
+        button_label = f"{daysConvert[day.strftime('%A')]} {day.strftime('%d')}"  # Example: "Monday 28"
+        if cols[i].button(button_label):
+            selected_day = daysConvert[day.strftime('%A')]
+    
+    # Display workout details for the selected day
+    if selected_day:
+        st.subheader("Workout of the Day")
+        selected_planning = planning[['WOD', selected_day]]
+        st.markdown("""
+            <style>
+                .card {
+                    border-radius: 10px;
+                    padding: 20px;
+                    margin-bottom: 15px;
+                    background-color: #2E3B4E;
+                    color: white;
+                    text-align: center;
+                    font-size: 18px;
+                    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.3);
+                }
+            </style>
+        """, unsafe_allow_html=True)
+        
+        # Creating vertical scrolling cards
+        for i in range(len(selected_planning)):
+            with st.container():
+                st.markdown(f"""
+                    <div class="card">
+                        <h2>{selected_planning.loc[i, "WOD"]}</h2>
+                        <p>{selected_planning.loc[i, selected_day]}</p>
+                    </div>
+                """, unsafe_allow_html=True)
+    
+    
+    st.divider()
+    
+    st.subheader("Inscription au cours via PollForAll :calendar: ")
+    components.iframe(url, height=800, scrolling = True)
