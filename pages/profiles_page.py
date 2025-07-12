6e2f8625151e69863e9353886d4f5852b4749467
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-07-12 15:01:27 +0200

    Update profiles_page.py

--- pages/profiles_page.py
+++ pages/profiles_page.py
@@ -12,7 +12,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import seaborn as sns
-from utils.functions import go_home, get_conn_and_df, highlight_rows, ChartDataFS
+from utils.functions import go_home, get_conn_and_df, highlight_rows, ChartDataFS, UpdateDB
     
 def data_perso(df) :
     athl = str(st.session_state.athl)
