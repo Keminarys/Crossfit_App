aa3156dcb4c1a83b7d4d831a8409bc6879feb425
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-07-13 15:46:56 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from utils.auth import login_ui, logout_ui, cookies
+from utils.auth import main_auth#login_ui, logout_ui, cookies
 from utils.functions import get_conn_and_df
 from utils.ui_helpers import render_navbar
 
@@ -14,29 +14,23 @@ from datetime import date
 st.set_page_config(layout="wide")
 
 def main():
-    if not cookies.ready():
-        st.stop()
-    
-    login_ui()
-    nav_col, logout_col = st.columns([8, 1])
+    if main_auth():
 
-    with nav_col:
         render_navbar([
             ("Votre Profil",    "profiles_page"),
             ("Votre Progression","progress"),
             ("Ressources Crossfit","ressources"),
             ("Programmation",   "scheduleResa"),
         ])
+    
 
-    with logout_col:
-        logout_ui()
-
-    title, logo = st.columns([3, 1])
-    with title:
-        st.title('Crossfit83 Le Beausset')
-        st.write(f"Bienvenue, {st.session_state.athl}!")
-    with logo:
-        st.image("LogoCrossfit.jpg")
+    
+        title, logo = st.columns([3, 1])
+        with title:
+            st.title('Crossfit83 Le Beausset')
+            st.write(f"Bienvenue, {st.session_state.athl}!")
+        with logo:
+            st.image("LogoCrossfit.jpg")
 
 if __name__ == "__main__":
     main()
