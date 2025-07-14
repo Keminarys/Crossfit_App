68b855f2c9b5a8ceeeccf6979550a8f6f0187757
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-07-14 12:53:23 +0200

    Update ressources.py

--- pages/ressources.py
+++ pages/ressources.py
@@ -13,6 +13,7 @@ import requests
 from bs4 import BeautifulSoup
 import streamlit.components.v1 as components
 from utils.functions import get_conn_and_df, WOD_crossfit, random_date_url, get_all_heroes, wodGirls
+#from utils.auth import login_ui, logout_ui
 from utils.auth import require_login, get_current_user, logout_button 
 from utils.ui_helpers import render_navbar    
 
