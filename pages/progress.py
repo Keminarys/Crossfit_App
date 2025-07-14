13947e4b3963ecc8002ae04e544207acac93b02a
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-07-14 10:38:38 +0200

    Update progress.py

--- pages/progress.py
+++ pages/progress.py
@@ -7,15 +7,24 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 from datetime import date
 from utils.functions import get_conn_and_df
-#from utils.auth import login_ui, logout_ui
+from utils.auth import login_ui, logout_ui
 from utils.ui_helpers import render_navbar    
 
-render_navbar([
-    ("Votre Profil",    "profiles_page"),
-    ("Votre Progression","progress"),
-    ("Ressources Crossfit","ressources"),
-    ("Programmation",   "scheduleResa"),
-])
+if not st.session_state.get("authenticated"):
+    login_ui()
+    
+nav_col, logout_col = st.columns([8, 1])
+
+with nav_col:
+    render_navbar([
+        ("Votre Profil",    "profiles_page"),
+        ("Votre Progression","progress"),
+        ("Ressources Crossfit","ressources"),
+        ("Programmation",   "scheduleResa"),
+    ])
+
+with logout_col:
+    logout_ui()
 
 
 
