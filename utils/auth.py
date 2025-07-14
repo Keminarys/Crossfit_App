2ae4d8ac04a6cae55c3fed10f66fcd6ed43bb8a8
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-07-14 12:24:01 +0200

    Update auth.py

--- utils/auth.py
+++ utils/auth.py
@@ -1,76 +1,212 @@
+# import streamlit as st
+# import hashlib
+# import uuid
+# from streamlit_gsheets import GSheetsConnection
+# from streamlit_cookies_manager import EncryptedCookieManager
+# from utils.functions import get_conn_and_df, UpdateDB
+# import streamlit_cookies_manager
+# from datetime import datetime, timedelta
+
+
+# # ---------------------------------------------------------------------------- #
+# # Changing expiry from cookie created
+# # ---------------------------------------------------------------------------- #
+
+# _original_init = streamlit_cookies_manager.CookieManager.__init__
+
+# def _patched_init(self, *args, expiry_min=10, **kwargs):
+#     _original_init(self, *args, **kwargs)
+#     self._default_expiry = datetime.now() + timedelta(minutes=expiry_min)
+
+# streamlit_cookies_manager.CookieManager.__init__ = _patched_init
+
+# # Now all CookieManager (and thus EncryptedCookieManager) use 10 mins by default
+
+# # ---------------------------------------------------------------------------- #
+# # 0. Per-session unique ID
+# # ---------------------------------------------------------------------------- #
+# if "session_id" not in st.session_state:
+#     st.session_state.session_id = str(uuid.uuid4())
+
+# # Load secret for cookie encryption
+# x = get_conn_and_df("Credentials")
+# x_pass = x.at[x['username'].eq('COOKIES_SECRET').idxmax(), 'password']
+
+# cookies = EncryptedCookieManager(
+#     prefix=f"crossfit83/{st.session_state.session_id}/",
+#     password=x_pass
+# )
+# if not cookies.ready():
+#     st.stop()
+
+# # ---------------------------------------------------------------------------- #
+# # 1. Load user DB
+# # ---------------------------------------------------------------------------- #
+# def load_user_db():
+#     return get_conn_and_df("Credentials")
+    
+# # ---------------------------------------------------------------------------- #
+# # 2. Hashing
+# # ---------------------------------------------------------------------------- #
+# def hash_password(password: str) -> str:
+#     return hashlib.sha256(password.encode()).hexdigest()
+
+# # ---------------------------------------------------------------------------- #
+# # 3. Auth Dialog
+# # ---------------------------------------------------------------------------- #
+# @st.dialog("🔐 Authentication")
+# def _auth_dialog():
+#     mode = st.radio("Choose action", ["Log In", "Sign Up"], horizontal=True)
+
+#     if mode == "Log In":
+#         user = st.text_input("Username")
+#         pw   = st.text_input("Password", type="password")
+
+#         if st.button("Login", type="primary"):
+#             db = load_user_db()
+#             is_valid = (
+#                 user in list(db.username) and
+#                 db.at[db['username'].eq(user).idxmax(), 'password'] == hash_password(pw)
+#             )
+#             if is_valid:
+#                 st.session_state.authenticated = True
+#                 st.session_state.athl = user
+#                 cookies["athl"] = user
+#                 cookies.save()
+#                 st.rerun()
+#             else:
+#                 st.error("Invalid username or password")
+
+#     else:  # Sign Up
+#         new_user = st.text_input("New Username")
+#         pw1      = st.text_input("Password", type="password")
+#         pw2      = st.text_input("Repeat Password", type="password")
+
+#         if st.button("Sign Up", type="primary"):
+#             db = load_user_db()
+#             if not new_user or not pw1:
+#                 st.error("All fields are required")
+#             elif new_user in list(db.username):
+#                 st.error("Username already exists")
+#             elif pw1 != pw2:
+#                 st.error("Passwords do not match")
+#             else:
+#                 password_hashed = hash_password(pw1)
+#                 UpdateDB(db, {'username': new_user, 'password': password_hashed}, "Credentials")
+#                 st.session_state.authenticated = True
+#                 st.session_state.athl = new_user
+#                 cookies["athl"] = new_user
+#                 cookies.save()
+#                 st.rerun()
+
+# def login_ui():
+#     if "authenticated" not in st.session_state:
+#         if cookies.get("athl"):
+#             st.session_state.authenticated = True
+#             st.session_state.athl = cookies.get("athl")
+#         else:
+#             st.session_state.authenticated = False
+
+#     if not st.session_state.authenticated:
+#         _auth_dialog()
+#         st.stop()
+
+# def logout_ui():
+#     if st.session_state.authenticated == True:
+#         if st.button("Logout", key="btn_logout"):
+#             st.session_state.authenticated = False
+#             if "athl" in cookies:
+#                 del cookies["athl"]
+#             cookies.save()
+#             st.session_state.pop('athl', None)
+#             st.session_state.pop('CookieManager.queue', None)
+#             st.cache_data.clear()
+#             st.rerun()
+#             log_in()
+
 import streamlit as st
 import hashlib
 import uuid
-from streamlit_gsheets import GSheetsConnection
-from streamlit_cookies_manager import EncryptedCookieManager
-from utils.functions import get_conn_and_df, UpdateDB
-import streamlit_cookies_manager
 from datetime import datetime, timedelta
 
+import streamlit_cookies_manager
+from streamlit_cookies_manager import CookieManager, EncryptedCookieManager
 
-# ---------------------------------------------------------------------------- #
-# Changing expiry from cookie created
-# ---------------------------------------------------------------------------- #
+from utils.functions import get_conn_and_df, UpdateDB
 
+
+# -----------------------------------------------------------------------------
+# Patch CookieManager to default to 10-minute expiry
+# -----------------------------------------------------------------------------
 _original_init = streamlit_cookies_manager.CookieManager.__init__
-
 def _patched_init(self, *args, expiry_min=10, **kwargs):
     _original_init(self, *args, **kwargs)
     self._default_expiry = datetime.now() + timedelta(minutes=expiry_min)
 
 streamlit_cookies_manager.CookieManager.__init__ = _patched_init
 
-# Now all CookieManager (and thus EncryptedCookieManager) use 10 mins by default
 
-# ---------------------------------------------------------------------------- #
-# 0. Per-session unique ID
-# ---------------------------------------------------------------------------- #
-if "session_id" not in st.session_state:
-    st.session_state.session_id = str(uuid.uuid4())
+# -----------------------------------------------------------------------------
+# 1. Generate or retrieve session_id via a raw CookieManager
+# -----------------------------------------------------------------------------
+raw_cm = CookieManager(prefix="crossfit83/")  
+if not raw_cm.ready():
+    st.stop()
 
-# Load secret for cookie encryption
-x = get_conn_and_df("Credentials")
-x_pass = x.at[x['username'].eq('COOKIES_SECRET').idxmax(), 'password']
+session_id = raw_cm.get("session_id")
+if not session_id:
+    session_id = str(uuid.uuid4())
+    raw_cm["session_id"] = session_id
+    raw_cm.save()
+
+
+# -----------------------------------------------------------------------------
+# 2. Load encryption secret and create your EncryptedCookieManager
+# -----------------------------------------------------------------------------
+_creds = get_conn_and_df("Credentials")
+_secret = _creds.loc[_creds.username.eq("COOKIES_SECRET"), "password"].iat[0]
 
 cookies = EncryptedCookieManager(
-    prefix=f"crossfit83/{st.session_state.session_id}/",
-    password=x_pass
+    prefix=f"crossfit83/{session_id}/",
+    password=_secret
 )
 if not cookies.ready():
     st.stop()
 
-# ---------------------------------------------------------------------------- #
-# 1. Load user DB
-# ---------------------------------------------------------------------------- #
+
+# -----------------------------------------------------------------------------
+# 3. Helpers
+# -----------------------------------------------------------------------------
+def hash_password(pw: str) -> str:
+    return hashlib.sha256(pw.encode()).hexdigest()
+
 def load_user_db():
     return get_conn_and_df("Credentials")
-    
-# ---------------------------------------------------------------------------- #
-# 2. Hashing
-# ---------------------------------------------------------------------------- #
-def hash_password(password: str) -> str:
-    return hashlib.sha256(password.encode()).hexdigest()
 
-# ---------------------------------------------------------------------------- #
-# 3. Auth Dialog
-# ---------------------------------------------------------------------------- #
+def get_current_user() -> str | None:
+    return cookies.get("athl")
+
+def is_authenticated() -> bool:
+    return get_current_user() is not None
+
+
+# -----------------------------------------------------------------------------
+# 4. Auth Dialog
+# -----------------------------------------------------------------------------
 @st.dialog("🔐 Authentication")
 def _auth_dialog():
-    mode = st.radio("Choose action", ["Log In", "Sign Up"], horizontal=True)
+    choice = st.radio("Choose action", ["Log In", "Sign Up"], horizontal=True)
 
-    if mode == "Log In":
+    if choice == "Log In":
         user = st.text_input("Username")
         pw   = st.text_input("Password", type="password")
-
         if st.button("Login", type="primary"):
             db = load_user_db()
-            is_valid = (
-                user in list(db.username) and
-                db.at[db['username'].eq(user).idxmax(), 'password'] == hash_password(pw)
-            )
-            if is_valid:
-                st.session_state.authenticated = True
-                st.session_state.athl = user
+            if (
+                user in db.username.values
+                and db.loc[db.username.eq(user), "password"].iat[0]
+                    == hash_password(pw)
+            ):
                 cookies["athl"] = user
                 cookies.save()
                 st.rerun()
@@ -78,48 +214,37 @@ def _auth_dialog():
                 st.error("Invalid username or password")
 
     else:  # Sign Up
-        new_user = st.text_input("New Username")
-        pw1      = st.text_input("Password", type="password")
-        pw2      = st.text_input("Repeat Password", type="password")
-
+        new_u = st.text_input("New Username")
+        pw1   = st.text_input("Password", type="password")
+        pw2   = st.text_input("Repeat Password", type="password")
         if st.button("Sign Up", type="primary"):
             db = load_user_db()
-            if not new_user or not pw1:
+            if not new_u or not pw1:
                 st.error("All fields are required")
-            elif new_user in list(db.username):
+            elif new_u in db.username.values:
                 st.error("Username already exists")
             elif pw1 != pw2:
                 st.error("Passwords do not match")
             else:
-                password_hashed = hash_password(pw1)
-                UpdateDB(db, {'username': new_user, 'password': password_hashed}, "Credentials")
-                st.session_state.authenticated = True
-                st.session_state.athl = new_user
-                cookies["athl"] = new_user
+                pwd_h = hash_password(pw1)
+                UpdateDB(db,
+                         {"username": new_u, "password": pwd_h},
+                         sheet_name="Credentials")
+                cookies["athl"] = new_u
                 cookies.save()
                 st.rerun()
 
-def login_ui():
-    if "authenticated" not in st.session_state:
-        if cookies.get("athl"):
-            st.session_state.authenticated = True
-            st.session_state.athl = cookies.get("athl")
-        else:
-            st.session_state.authenticated = False
 
-    if not st.session_state.authenticated:
+# -----------------------------------------------------------------------------
+# 5. Login / Logout helpers
+# -----------------------------------------------------------------------------
+def require_login():
+    if not is_authenticated():
         _auth_dialog()
         st.stop()
 
-def logout_ui():
-    if st.session_state.authenticated == True:
-        if st.button("Logout", key="btn_logout"):
-            st.session_state.authenticated = False
-            if "athl" in cookies:
-                del cookies["athl"]
-            cookies.save()
-            st.session_state.pop('athl', None)
-            st.session_state.pop('CookieManager.queue', None)
-            st.cache_data.clear()
-            st.rerun()
-            log_in()
+def logout_button():
+    if is_authenticated() and st.button("Logout"):
+        del cookies["athl"]
+        cookies.save()
+        st.rerun()
