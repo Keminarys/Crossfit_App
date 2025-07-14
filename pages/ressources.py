d92bbe984230a6bd8dd84dac34f7d765d5dc72a7
Keminarys <115616113+Keminarys@users.noreply.github.com>
2025-07-14 15:06:40 +0200

    Update ressources.py

--- pages/ressources.py
+++ pages/ressources.py
@@ -22,8 +22,8 @@ nav_col, logout_col = st.columns([8, 1])
 
 with nav_col:
     render_navbar([
+        ("Votre Profil",    "profiles_page"),
         ("Votre Progression","progress"),
-        ("Ressources Crossfit","ressources"),
         ("Programmation",   "scheduleResa"),
     ])
 
