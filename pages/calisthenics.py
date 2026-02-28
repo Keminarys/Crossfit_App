d260ec58e768b6c59830beeda46c9471787772ab
Keminarys <115616113+Keminarys@users.noreply.github.com>
2026-02-28 15:56:30 +0100

    Update calisthenics.py

--- pages/calisthenics.py
+++ pages/calisthenics.py
@@ -10,46 +10,53 @@ from googleapiclient.discovery import build
 creds = service_account.Credentials.from_service_account_info( st.secrets["connections"]["gsheets"], scopes=["https://www.googleapis.com/auth/drive.readonly"] ) 
 drive = build("drive", "v3", credentials=creds) 
 file_id = st.secrets["drive"]["json_file_id"] 
+info = drive.files().get(
+    fileId=file_id,
+    fields="id, name, mimeType, driveId, parents"
+).execute()
 
-request = drive.files().get_media(fileId=file_id) 
-content = request.execute() 
-data = json.loads(content)
+st.write(info)
 
-testnode = data[0]['movements']
-elements = []
-for item in testnode:
-    elements.append({"data": {"id": item["id"], "label": item["name"]}})
-    for nxt in item.get("progressions_to", []):
-        elements.append({"data": {"source": item["id"], "target": nxt}})
 
-app = dash.Dash(__name__)
-app.layout = html.Div([
-    cyto.Cytoscape(
-        id="skill-tree",
-        elements=elements,
-        layout={"name": "breadthfirst"},
-        style={"width": "100%", "height": "800px"},
-        stylesheet=[
-            {"selector": "node", "style": {"background-color": "#88c", "label": "data(label)"}},
-            {"selector": ".mastered", "style": {"background-color": "green"}}
-        ]
-    )
-])
+# request = drive.files().get_media(fileId=file_id) 
+# content = request.execute() 
+# data = json.loads(content)
 
-# Run Dash inside Streamlit
-from dash.dependencies import Input, Output, State
+# testnode = data[0]['movements']
+# elements = []
+# for item in testnode:
+#     elements.append({"data": {"id": item["id"], "label": item["name"]}})
+#     for nxt in item.get("progressions_to", []):
+#         elements.append({"data": {"source": item["id"], "target": nxt}})
 
-@app.callback(
-    Output("skill-tree", "elements"),
-    Input("skill-tree", "tapNodeData"),
-    State("skill-tree", "elements")
-)
-def mark_mastered(node, elements):
-    if node:
-        for el in elements:
-            if el.get("data", {}).get("id") == node["id"]:
-                el["classes"] = "mastered"
-    return elements
+# app = dash.Dash(__name__)
+# app.layout = html.Div([
+#     cyto.Cytoscape(
+#         id="skill-tree",
+#         elements=elements,
+#         layout={"name": "breadthfirst"},
+#         style={"width": "100%", "height": "800px"},
+#         stylesheet=[
+#             {"selector": "node", "style": {"background-color": "#88c", "label": "data(label)"}},
+#             {"selector": ".mastered", "style": {"background-color": "green"}}
+#         ]
+#     )
+# ])
 
-# Embed
-st.components.v1.html(app.index(), height=900, scrolling=True)
+# # Run Dash inside Streamlit
+# from dash.dependencies import Input, Output, State
+
+# @app.callback(
+#     Output("skill-tree", "elements"),
+#     Input("skill-tree", "tapNodeData"),
+#     State("skill-tree", "elements")
+# )
+# def mark_mastered(node, elements):
+#     if node:
+#         for el in elements:
+#             if el.get("data", {}).get("id") == node["id"]:
+#                 el["classes"] = "mastered"
+#     return elements
+
+# # Embed
+# st.components.v1.html(app.index(), height=900, scrolling=True)
