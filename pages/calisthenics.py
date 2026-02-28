1aa8ea35d055fdfa5b941599b3ccf73d9ea1cfdc
Keminarys <115616113+Keminarys@users.noreply.github.com>
2026-02-28 16:45:44 +0100

    Update calisthenics.py

--- pages/calisthenics.py
+++ pages/calisthenics.py
@@ -5,8 +5,9 @@ import dash
 import dash_cytoscape as cyto
 from dash import html
 from google.oauth2 import service_account 
-from utils.functions import load_drive_json
+from utils.functions import get_conn_and_df, load_drive_json
 from utils.ui_helpers import render_navbar
+from streamlit_cytoscapejs import cytoscape
 
 if not st.user.is_logged_in:
         if st.button("Log in with Google"):
@@ -34,3 +35,57 @@ if st.user.is_logged_in :
         file_id = st.secrets["drive"]["json_file_id"]
         data = load_drive_json(file_id, creds)
         
+if st.user.is_logged_in:
+        st.set_page_config(layout="wide")
+        athl = newName()
+        creds = service_account.Credentials.from_service_account_info(
+        st.secrets["connections"]["gsheets"],
+        scopes=["https://www.googleapis.com/auth/drive"]
+        )
+        file_id = st.secrets["drive"]["json_file_id"]
+        data = load_drive_json(file_id, creds)
+        movements = data[0]["movements"]
+        progress_data = get_conn_and_df("calistenicPathway")
+        try:
+                progress_data_id = progress_data.loc[progress_data.id == athl].to_dict()
+        except:
+                progress_data = {"id": athl, "mastered": []}
+        
+        mastered = set(progress_data["mastered"])
+        if "selected_nodes" not in st.session_state:
+                st.session_state.selected_nodes = set(mastered)
+        elements = []
+        for item in movements:
+                node_id = item["id"]
+                node_classes = "mastered" if node_id in st.session_state.selected_nodes else ""
+        
+                elements.append({
+                    "data": {"id": node_id, "label": item["name"]},
+                    "classes": node_classes
+                })
+                
+        for nxt in item.get("progressions_to", []):
+                elements.append({
+                "data": {"source": node_id, "target": nxt}})
+                st.subheader("Votre Skill Tree")
+                clicked = cytoscape(
+                        elements=elements,
+                        layout={"name": "breadthfirst"},
+                        stylesheet=[
+                            {"selector": "node", "style": {"background-color": "#88c", "label": "data(label)"}},
+                            {"selector": ".mastered", "style": {"background-color": "green"}}
+                        ],
+                        key="skilltree")
+        if clicked and "id" in clicked:
+                node_id = clicked["id"]
+        
+        if node_id in st.session_state.selected_nodes:
+                st.session_state.selected_nodes.remove(node_id)
+        else:
+                st.session_state.selected_nodes.add(node_id)
+                st.rerun()
+        st.markdown("---")
+        if st.button("💾 Sauvegarder la progression"):
+                new_mastered = list(st.session_state.selected_nodes)
+                progressCalistenics(progress_data, athl, new_mastered, 'calistenicPathway')
+                st.success("Progression sauvegardée !")
