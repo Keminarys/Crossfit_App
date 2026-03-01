aa050272b7da78529548b4ec3fd61a29db1d758f
Keminarys <115616113+Keminarys@users.noreply.github.com>
2026-03-01 10:58:13 +0100

    Update calisthenics.py

--- pages/calisthenics.py
+++ pages/calisthenics.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests 
 import json 
 from google.oauth2 import service_account 
-from utils.functions import get_conn_and_df, load_drive_json, build_pyvis_tree, render_tree, get_clicked_node
+from utils.functions import get_conn_and_df, load_drive_json, build_pyvis_tree, render_tree
 from utils.ui_helpers import render_navbar
 import graphviz
 import tempfile
