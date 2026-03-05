be0d2197067b8ebb186a48c22ccec758b95d9a83
Keminarys <115616113+Keminarys@users.noreply.github.com>
2026-03-05 19:33:09 +0100

    Update allow.py

--- utils/allow.py
+++ utils/allow.py
@@ -10,10 +10,11 @@ def hmac_email(email: str) -> str:
     normalized = email.strip().lower().encode()
     return hmac.new(secret_key.encode(), normalized, hashlib.sha256).hexdigest()
   
-def add_allowed_email(email: str, sheet_name: str):
+def add_allowed_email(email: str, role: str,sheet_name: str):
     df = get_conn_and_df(sheet_name)
     new_entry = {
-        "email_hmac": hmac_email(email)
+        "email_hmac": hmac_email(email), 
+        "role": role
     }
     UpdateDB(df, new_entry, sheet_name)
   
@@ -23,3 +24,14 @@ def is_email_allowed(email: str, sheet_name: str) -> bool:
         return False
     hashed = hmac_email(email)
     return hashed in df["email_hmac"].astype(str).values
+
+def get_user_role(email: str, sheet_name: str):
+    df = get_conn_and_df(sheet_name)
+    if df.empty:
+        return False, None
+    hashed = hmac_email(email)
+    match = df[df["email_hmac"] == hashed]
+    if match.empty:
+        return False, None
+    role = match.iloc[0]["role"]
+    return True, role
