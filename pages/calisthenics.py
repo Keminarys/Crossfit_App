b5b9a3ddf14f1433ca04ea2abb3242adf5fa7cfa
Keminarys <115616113+Keminarys@users.noreply.github.com>
2026-03-05 19:51:52 +0100

    Update calisthenics.py

--- pages/calisthenics.py
+++ pages/calisthenics.py
@@ -3,6 +3,7 @@ import requests
 import json 
 from google.oauth2 import service_account 
 from utils.functions import get_conn_and_df, load_drive_json, render_tree, newName
+from utils.allow import is_email_allowed, get_user_role, add_allowed_email
 from utils.ui_helpers import render_navbar
 import tempfile
 import streamlit.components.v1 as components
@@ -27,31 +28,38 @@ with log:
             st.logout()
 
 if st.user.is_logged_in :
-        st.set_page_config(layout="wide")
-        creds = service_account.Credentials.from_service_account_info(
-        st.secrets["connections"]["gsheets"],
-        scopes=["https://www.googleapis.com/auth/drive.readonly"]
-        )
-        file_id = st.secrets["drive"]["json_file_id"]
-        data = load_drive_json(file_id, creds)
-        all_tree_list = []
-        mastered_df = get_conn_and_df("calistenicPathway")
-        athl = newName()
-        for i in range(0,len(data)):
-          tree = data[i]['movements'][0]['skill_tree_links']
-          all_tree_list.append(tree[0])
-        st.title(f"Bienvenue sur la page spéciale Calisthénie {athl}!")
-        st.divider()
-        if athl in mastered_df.id.unique().tolist() : 
-                 progressState = mastered_df[mastered_df['id'] == athl].iloc[0].to_dict()
+        user_email = st.user.email
+        allowed, role = get_user_role(user_email, "allowList")
+        if not allowed:
+            st.error("Authenticated but NOT authorized.")
+            st.stop()
+            
         else : 
-                 progressState = {'id' : athl, 
-                   'mastered' : []}
-        selected_tree = st.selectbox("Quel arbre de compétence voulez vous voir ?", all_tree_list)
-        if len(selected_tree) > 0 :
-                 idx_skill_tree = all_tree_list.index(selected_tree)
-                 movements = data[idx_skill_tree]["movements"]
-                 st.subheader(f"Arbre interactif : {selected_tree}")
-                 render_tree(movements)
+                 st.set_page_config(layout="wide")
+                 creds = service_account.Credentials.from_service_account_info(
+                 st.secrets["connections"]["gsheets"],
+                 scopes=["https://www.googleapis.com/auth/drive.readonly"]
+                 )
+                 file_id = st.secrets["drive"]["json_file_id"]
+                 data = load_drive_json(file_id, creds)
+                 all_tree_list = []
+                 mastered_df = get_conn_and_df("calistenicPathway")
+                 athl = newName()
+                 for i in range(0,len(data)):
+                   tree = data[i]['movements'][0]['skill_tree_links']
+                   all_tree_list.append(tree[0])
+                 st.title(f"Bienvenue sur la page spéciale Calisthénie {athl}!")
+                 st.divider()
+                 if athl in mastered_df.id.unique().tolist() : 
+                          progressState = mastered_df[mastered_df['id'] == athl].iloc[0].to_dict()
+                 else : 
+                          progressState = {'id' : athl, 
+                            'mastered' : []}
+                 selected_tree = st.selectbox("Quel arbre de compétence voulez vous voir ?", all_tree_list)
+                 if len(selected_tree) > 0 :
+                          idx_skill_tree = all_tree_list.index(selected_tree)
+                          movements = data[idx_skill_tree]["movements"]
+                          st.subheader(f"Arbre interactif : {selected_tree}")
+                          render_tree(movements)
 
 
