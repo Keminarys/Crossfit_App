3d5819a898ee91845ae4afff606833895235d66b
Keminarys <115616113+Keminarys@users.noreply.github.com>
2026-03-05 19:28:31 +0100

    Update allow.py

--- utils/allow.py
+++ utils/allow.py
@@ -1,5 +1,9 @@
 import hmac, hashlib
 from utils.functions import get_conn_and_df, UpdateDB
+import streamlit as st
+import pandas as pd
+from streamlit_gsheets import GSheetsConnection
+import numpy as np
 
 def hmac_email(email: str) -> str:
     secret_key = st.secrets["key_h"]["allowlist_hmac_key"]
