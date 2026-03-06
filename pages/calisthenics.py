eb7b70cacc711d773ca03c455f5d52bc9af57631
Keminarys <115616113+Keminarys@users.noreply.github.com>
2026-03-06 10:06:39 +0100

    Update calisthenics.py

--- pages/calisthenics.py
+++ pages/calisthenics.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests 
 import json 
 from google.oauth2 import service_account 
-from utils.functions import get_conn_and_df, load_drive_json, render_tree, newName
+from utils.functions import get_conn_and_df, load_drive_json, render_tree, newName, show_calisthenics_tab
 from utils.allow import is_email_allowed, get_user_role, add_allowed_email
 from utils.ui_helpers import render_navbar
 import tempfile
@@ -60,6 +60,6 @@ if st.user.is_logged_in :
                           idx_skill_tree = all_tree_list.index(selected_tree)
                           movements = data[idx_skill_tree]["movements"]
                           st.subheader(f"Arbre interactif : {selected_tree}")
-                          render_tree(movements)
+                          show_calisthenics_tab(movements)
 
 
